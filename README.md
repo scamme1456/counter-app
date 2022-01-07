34858fb565dd0cb766b3067a7aa105d391a48c23
a-lukasiewicz <adamlukasiewicz3@gmail.com>
2022-01-07 19:37:15 +0100

    basic frontend done

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # clocker-AI-Project
+
+# Run live server 
+php -S 127.0.0.1:8000/filename.php
---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+
+$dbhost = "dbhost";
+$dbuser = "user";
+$dbpass = "123";
+$dbname = "db_name";
+
+// if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
+// {
+// 	die("Failed to connect!");
+// }
