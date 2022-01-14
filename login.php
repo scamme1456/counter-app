b93a425794dd895e66a63da9b7cb6de9f4a0c2b7
Mona Lisa <ka46561@zut.edu.pl>
2022-01-14 18:00:59 +0100

    poprawka

--- login.php
+++ login.php
@@ -40,7 +40,7 @@ session_start();
 							die;
 						}
 						$_SESSION['user_id'] = $user_data['user_id'];
-						//header("Location: index.php");
+						header("Location: index.php");
 						die;
 					}
 				}
