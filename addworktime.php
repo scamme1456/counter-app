be68c115da3d7725bdf80b49e2bfd4a874eba2d1
Mona Lisa <ka46561@zut.edu.pl>
2022-01-14 22:47:47 +0100

    dodawanie rekordow pracy

--- addworktime.php
+++ addworktime.php
@@ -3,6 +3,31 @@ session_start();
 	include("config/connection.php");
 	include("config/functions.php");
 
+    if($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+		//something was posted
+		$start_work = $_POST['start_work'];
+        $stop_work = $_POST['stop_work'];
+    	$description = $_POST['description'];
+        $user_id = $_SESSION['user_id'];
+
+
+		if(!empty($start_work) && !empty($stop_work) && !empty($description))
+		{
+
+			$company_id = random_num(20);
+            $workrecord_id = random_num(20);
+			$query = "insert into workrecord(workrecord_id,worker_id,start_work,stop_work,description,date) values('$workrecord_id','$user_id','$start_work','$stop_work','$description', NOW());";
+			
+			mysqli_query($con, $query);
+			header("Location: user_index.php");
+			die;
+		}
+		else
+		{
+			echo "Please enter some valid information!";
+		}
+	}
   
 ?>
 
@@ -16,8 +41,8 @@ session_start();
     <div id="box">
       <form method="post" id="login">
         <h1 id="title">Add new work record</h1>
-        <input id="text" type="text" name="start_work" placeholder="Work start time" />
-        <input id="text" type="text" name="stop_work" placeholder="Work stop time" />
+        <input id="text" type="text" name="start_work" placeholder="Work start time e.g. 12-01-2012 21:24:00" />
+        <input id="text" type="text" name="stop_work" placeholder="Work stop time e.g. 12-01-2012 23:24:00" />
         <input id="text" type="text" name="description" placeholder="Description" />
 		<input id="button" type="submit" value="Add">
 		<a href="user_index.php"></a> 
