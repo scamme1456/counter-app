6ac0891aeee16ab2fd3aaf94397d6de7591ef9f8
scamme1456 <scamme1456@gmail.com>
2022-04-28 00:56:37 +0200

    1:01:00

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,33 @@ import React, { Component } from "react";
 
 class Counter extends Component {
     state = {
-        count:0
+        count:0,
+        tags: []
     };
 
+    renderTag(){
+      if(this.state.tags.length === 0) return <p>Niestety, to nie jest lista.</p>;
+      
+      return <ul>
+          { this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+      </ul>
+    }
+
+
   render() {
-    return (
-      <React.Fragment>
-        <span>Actual value: {this.formatCount()}</span>
-        <button>Increment</button>
-      </React.Fragment>
-    );
+
+    return <div>
+      {this.state.tags.length === 0 && 'we no'}
+      {this.renderTag()}
+      </div>;
+
+  }
+
+/*
+  getBadgeClasses() {
+    let classes = "badge m-2 bg-";
+    classes += (this.state.count === 0) ? "warning" : "primary";
+    return classes;
   }
 
   formatCount(){
@@ -19,5 +36,5 @@ class Counter extends Component {
       return count === 0 ? 'Zero' : count;
   }
 }
-
+*/}
 export default Counter;
